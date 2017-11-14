c326860d80a838f3779afd0c4eba19c59e89c82e
zeruel11 <the.zero0110@gmail.com>
2017-11-14 18:46:57 +0700

    login url change

--- application/views/beranda_view.php
+++ application/views/beranda_view.php
@@ -5,4 +5,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 	<h1>Hello World</h1>
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p><a href="<?php echo base_url(); ?>index.php/login">Login</a></p>
 </body>
