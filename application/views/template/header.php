8553408c871e458d1188f1a29649b6bef2042ef7
zeruel11 <the.zero0110@gmail.com>
2018-01-16 20:42:23 +0700

    change icon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -11,24 +11,11 @@
 	<!-- css imports -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/leaflet.css" />
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css"/>
-
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/grid.css" />
 
 	<!-- core javascript import -->
 	<script src="<?php echo base_url(); ?>assets/js/leaflet.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js" type="text/javascript"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-
-	<style>
-
-	#image-map {
-		width: 100%;
-		height: 500px;
-		border: 3px solid #ccc;
-		/*margin-bottom: 10px;*/
-	}
-
-</style>
-
 
 </head>
